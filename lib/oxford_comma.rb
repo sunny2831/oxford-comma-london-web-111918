66e90f1f824cd8b68014d99e21669873e359e837
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-07 11:46:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   elsif array.length >= 3
-    array[-1] = " and #{array[-1]}"  
+    array[-1] = " and #{array[-1]}"
     array.join(", ")
   end
 end
